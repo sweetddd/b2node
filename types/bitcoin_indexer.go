bbec7bb8dcbda6cad7ecfb354c4841845ff2aa58
oxf71 <144757737+oxf71@users.noreply.github.com>
2023-11-18 10:43:32 +0800

    feat: parse bitcoin tx
    * add BITCOINTxIndexer interface
    * add parse block and tx logic
    * add proto file bitcoin_indexer.proto
    * bitcoin_indexer proto add BitcoinTxParseResult message

---
+++ types/bitcoin_indexer.go
@@ -0,0 +1,7 @@
+package types
+
+// BITCOINTxIndexer defines the interface of custom bitcoin tx indexer.
+type BITCOINTxIndexer interface {
+	// ParseBlock parse bitcoin block tx
+	ParseBlock(int64) ([]BitcoinTxParseResult, error)
+}
