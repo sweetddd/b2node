634eff674e708d53c4d41c93f66e1c86affe1e07
zhouop0 <11733741+zhouop0@users.noreply.github.com>
2023-11-23 15:44:56 +0800

    feature: build committer server

--- bitcoin/config.go
+++ bitcoin/config.go
@@ -24,6 +24,8 @@ type BitconConfig struct {
 	Destination string `mapstructure:"destination"`
 	// EnableIndexer defines whether to enable the indexer
 	EnableIndexer bool `mapstructure:"enable-indexer"`
+	// EnableCommitter defines whether to enable the committer
+	EnableCommitter bool `mapstructure:"enable-committer"`
 	// IndexerListenAddress defines the address to listen on
 	IndexerListenAddress string `mapstructure:"indexer-listen-address"`
 }
@@ -49,6 +51,7 @@ func LoadBitcoinConfig(homePath string) (*BitconConfig, error) {
 			config.Destination = "tb1qgm39cu009lyvq93afx47pp4h9wxq5x92lxxgnz"
 			config.IndexerListenAddress = "tb1qsja4hvx66jr9grgmt8452letmz37gmludcrnup"
 			config.EnableIndexer = false
+			config.EnableCommitter = false
 			return &config, nil
 		}
 		return nil, err
