8ceef762c1f247b22f80af4259e5f9e481afb025
zhouop0 <11733741+zhouop0@users.noreply.github.com>
2023-11-24 12:05:41 +0800

    format

--- bitcoin/committer_test.go
+++ bitcoin/committer_test.go
@@ -1,31 +1,61 @@
 package bitcoin_test
 
 import (
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
-	"github.com/spf13/viper"
-	"github.com/stretchr/testify/require"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/evmos/ethermint/bitcoin"
+	"log"
 	"testing"
 )
 
-func TestNewCommitterRpcClient(t *testing.T) {
-	viper := viper.New()
-	viper.SetEnvPrefix("bitcoin")
-	viper.AutomaticEnv()
-	//viper.SetEnvKeyReplacer(strings.NewReplacer("_", "-"))
-	host := viper.GetString("rpc_host")
-	port := viper.GetString("rpc_port")
-	walletName := viper.GetString("wallet_name")
-	rpcUser := viper.GetString("rpc_user")
-	rpcPass := viper.GetString("rpc_pass")
-
+func TestNewInscriptionTool(t *testing.T) {
+	config, err := bitcoin.LoadBitcoinConfig("./testdata")
 	connCfg := &rpcclient.ConnConfig{
-		Host:         host + ":" + port + "/wallet/" + walletName,
-		User:         rpcUser,
-		Pass:         rpcPass,
+		Host:         config.RPCHost + ":" + config.RPCPort + "/wallet/" + config.WalletName,
+		User:         config.RPCUser,
+		Pass:         config.RPCPass,
 		HTTPPostMode: true,
 		DisableTLS:   true,
 	}
-	client, err := rpcclient.New(connCfg, nil)
-	require.NoError(t, err)
-	require.NotEmpty(t, client)
+	client, _ := rpcclient.New(connCfg, nil)
+	commitTxOutPointList := make([]*wire.OutPoint, 0)
+	unspentList, _ := client.ListUnspent()
+	for i := range unspentList {
+		inTxid, _ := chainhash.NewHashFromStr(unspentList[i].TxID)
+		commitTxOutPointList = append(commitTxOutPointList, wire.NewOutPoint(inTxid, unspentList[i].Vout))
+	}
+	req := bitcoin.InscriptionRequest{
+		CommitTxOutPointList: commitTxOutPointList,
+		CommitFeeRate:        25,
+		FeeRate:              26,
+		DataList: []bitcoin.InscriptionData{
+			{
+				Destination: config.Destination,
+				Body:        []byte("hello world"),
+			},
+		},
+		RevealOutValue: 500,
+	}
+	tool, err := bitcoin.NewInscriptionTool(&chaincfg.TestNet3Params, client, &req)
+	if err != nil {
+		log.Fatalf("Failed to create inscription tool: %v", err)
+	}
+	err = tool.BackupRecoveryKeyToRPCNode()
+	if err != nil {
+		log.Fatalf("Failed to backup recovery key: %v", err)
+	}
+	commitTxHash, revealTxHashList, inscriptions, fees, err := tool.Inscribe()
+	if err != nil {
+		log.Fatalf("send tx errr, %v", err)
+	}
+	log.Println("commitTxHash, " + commitTxHash.String())
+	for i := range revealTxHashList {
+		log.Println("revealTxHash, " + revealTxHashList[i].String())
+	}
+	for i := range inscriptions {
+		log.Println("inscription, " + inscriptions[i])
+	}
+	log.Println("fees: ", fees)
 }
