eef3d6dcad3ef22e81f45c236c5216dc17a82111
robertcc <robertcc@test.com>
2023-12-18 20:50:38 +0800

    modify evm listener server

--- bitcoin/config.go
+++ bitcoin/config.go
@@ -110,36 +110,71 @@ func LoadBitcoinConfig(homePath string) (*BitconConfig, error) {
 	if err != nil {
 		// if file not exist use default config
 		// TODO: add gen config command after, The default configuration may not be required
-		if os.IsNotExist(err) {
-			config.NetworkName = "signet"
-			config.RPCHost = "localhost"
-			config.RPCPort = "8332"
-			config.RPCUser = "user"
-			config.RPCPass = "password"
-			config.WalletName = "walletname"
-			config.Destination = "tb1qgm39cu009lyvq93afx47pp4h9wxq5x92lxxgnz"
-			config.IndexerListenAddress = "tb1qsja4hvx66jr9grgmt8452letmz37gmludcrnup"
-			config.EnableIndexer = false
-			config.EnableCommitter = false
-			return &config, nil
+		if !os.IsNotExist(err) {
+			return nil, err
 		}
-		return nil, err
+		//config.NetworkName = "signet"
+		//config.RPCHost = "localhost"
+		//config.RPCPort = "8332"
+		//config.RPCUser = "user"
+		//config.RPCPass = "password"
+		//config.WalletName = "walletname"
+		//config.Destination = "tb1qgm39cu009lyvq93afx47pp4h9wxq5x92lxxgnz"
+		//config.IndexerListenAddress = "tb1qsja4hvx66jr9grgmt8452letmz37gmludcrnup"
+		//config.EnableIndexer = false
+		//config.EnableCommitter = false
+		//return &config, nil
 	}
 
 	v := viper.New()
 	v.SetConfigFile(configFile)
-	v.AutomaticEnv()
 	v.SetEnvPrefix("BITCOIN")
 	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	v.AutomaticEnv()
 
 	if err := v.ReadInConfig(); err != nil {
-		return nil, err
+		if !os.IsNotExist(err) {
+			return nil, err
+		}
+		// Load from environment variables if not found in the config file
+		v.BindEnv("network-name", "BITCOIN_NETWORK_NAME")
+		v.BindEnv("rpc-host", "BITCOIN_RPC_HOST")
+		v.BindEnv("rpc-port", "BITCOIN_RPC_PORT")
+		v.BindEnv("rpc-user", "BITCOIN_RPC_USER")
+		v.BindEnv("rpc-pass", "BITCOIN_RPC_PASS")
+		v.BindEnv("wallet-name", "BITCOIN_WALLET_NAME")
+		v.BindEnv("destination", "BITCOIN_DESTINATION")
+		v.BindEnv("enable-indexer", "BITCOIN_ENABLE_INDEXER")
+		v.BindEnv("enable-committer", "BITCOIN_ENABLE_COMMITTER")
+		v.BindEnv("indexer-listen-address", "BITCOIN_INDEXER_LISTEN_ADDRESS")
+		v.BindEnv("bridge.eth-rpc-url", "BITCOIN_BRIDGE_ETH_RPC_URL")
+		v.BindEnv("bridge.eth-priv-key", "BITCOIN_BRIDGE_ETH_PRIV_KEY")
+		v.BindEnv("bridge.contract-address", "BITCOIN_BRIDGE_CONTRACT_ADDRESS")
+		v.BindEnv("bridge.abi", "BITCOIN_BRIDGE_ABI")
+		v.BindEnv("bridge.gas-limit", "BITCOIN_BRIDGE_GAS_LIMIT")
+		v.BindEnv("bridge.aa-sca-registry", "BITCOIN_BRIDGE_AA_SCA_REGISTRY")
+		v.BindEnv("bridge.aa-kernel-factory", "BITCOIN_BRIDGE_AA_KERNEL_FACTORY")
+		v.BindEnv("state.host", "BITCOIN_STATE_HOST")
+		v.BindEnv("state.port", "BITCOIN_STATE_PORT")
+		v.BindEnv("state.user", "BITCOIN_STATE_USER")
+		v.BindEnv("state.pass", "BITCOIN_STATE_PASS")
+		v.BindEnv("state.db-name", "BITCOIN_STATE_DB_NAME")
+		v.BindEnv("source-address", "BITCOIN_SOURCE_ADDRESS")
+		v.BindEnv("fee", "BITCOIN_FEE")
+		v.BindEnv("evm.enable-listener", "BITCOIN_EVM_ENABLE_LISTENER")
+		v.BindEnv("evm.rpc-host", "BITCOIN_EVM_RPC_HOST")
+		v.BindEnv("evm.rpc-port", "BITCOIN_EVM_RPC_PORT")
+		v.BindEnv("evm.contract-address", "BITCOIN_EVM_CONTRACT_ADDRESS")
+		v.BindEnv("evm.start-height", "BITCOIN_EVM_START_HEIGHT")
+		v.BindEnv("evm.deposit", "BITCOIN_EVM_DEPOSIT")
+		v.BindEnv("evm.withdraw", "BITCOIN_EVM_WITHDRAW")
 	}
 
 	err = v.Unmarshal(&config)
 	if err != nil {
 		return nil, err
 	}
+
 	return &config, nil
 }
 
