c153aab0fd0aa576b957fae39fb5d3ae7e944418
oxf71 <144757737+oxf71@users.noreply.github.com>
2023-12-19 18:16:57 +0800

    feat(indexer): bitcoin indexer add db store

--- types/bitcoin_indexer.go
+++ types/bitcoin_indexer.go
@@ -3,7 +3,7 @@ package types
 // BITCOINTxIndexer defines the interface of custom bitcoin tx indexer.
 type BITCOINTxIndexer interface {
 	// ParseBlock parse bitcoin block tx
-	ParseBlock(int64) ([]*BitcoinTxParseResult, error)
+	ParseBlock(int64, int64) ([]*BitcoinTxParseResult, error)
 	// LatestBlock get latest block height in the longest block chain.
 	LatestBlock() (int64, error)
 }
