f491d6de2a27b522b8455b03d517571864ddcc59
oxf71 <144757737+oxf71@users.noreply.github.com>
2023-12-25 17:46:45 +0800

    fix: fix bridge deposit

--- types/bitcoin_bridge.go
+++ types/bitcoin_bridge.go
@@ -3,5 +3,5 @@ package types
 // BITCOINBridge defines the interface of custom bitcoin bridge.
 type BITCOINBridge interface {
 	// Deposit transfers amout to address
-	Deposit(string, string, int64) ([]byte, error)
+	Deposit(string, string, int64) (string, error)
 }
